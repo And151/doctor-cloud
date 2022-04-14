8b5ca431efe579658659f983b7e72903cbeabe90
Andranik <andranik.abrahamyan@zappix.com>
2022-04-14 18:37:05 +0400

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 const corsOptions: CorsOptions = {
-  allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+  allowedHeaders: '*',
   origin: 'http://localhost:4200',
   credentials: true,
   methods: ['POST', 'GET', 'PATCH', 'PUT', 'DELETE']
